68ca277ca24a8fb8c1cf06a5a71827a4b44f9ea8
Joksa <nikolajoksimovic419@gmail.com>
2023-04-02 14:34:40 +0200

    fixed home import

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import './Styles/App.css'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './Pages/Home';
+import Home from './Pages/Home/Home';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='/' element={<Home />} />
+
       </Routes>
     </BrowserRouter>
   )
