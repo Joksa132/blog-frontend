d5cf8ebcde6889eb736b3c1aefedb07f984dcf7d
Joksa <nikolajoksimovic419@gmail.com>
2023-04-02 16:48:55 +0200

    Implmented Login and Register

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,17 @@
-import { Modal, Typography, Box, TextField, Button, IconButton } from "@mui/material"
+import { Modal, Typography, Box, TextField, Button, IconButton, CssBaseline } from "@mui/material"
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import CloseIcon from '@mui/icons-material/Close';
+import { ThemeProvider } from '@mui/material/styles';
 
-import { useState } from "react"
+import { useContext, useState } from "react"
+import axios from "axios";
+import { UserContext } from "../../Context/UserContext";
 
-function Login({ open, onClose }) {
+function Login({ open, onClose, theme }) {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('');
+  const { setUser } = useContext(UserContext)
 
   const onUsernameChange = (e) => {
     setUsername(e.target.value)
@@ -18,69 +23,94 @@ function Login({ open, onClose }) {
 
   const onSubmit = (e) => {
     e.preventDefault()
-    // to do
-    onClose();
+    const user = {
+      username,
+      password
+    }
+    axios.post(`http://localhost:4000/user/login`, user)
+      .then((res) => {
+        console.log(res.data)
+        setUser({
+          username: res.data.userInfo.username,
+          userId: res.data.userInfo.Id
+        })
+        localStorage.setItem("token", res.data.token)
+        onClose()
+      })
+      .catch((err) => {
+        console.log(err)
+        setError(err.response.data.message)
+      })
   }
 
   return (
-    <Modal open={open} onClose={onClose}>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: 400,
-          bgcolor: "background.paper",
-          border: "2px solid #000",
-          boxShadow: 24,
-          p: 2,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: "5px"
-        }}
-      >
-        <IconButton color="secondary" onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Modal open={open} onClose={onClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "5px"
+          }}
+        >
+          <IconButton color="secondary" onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
 
-        <Typography color="secondary" variant="h4" fontWeight="bold">Login</Typography>
-        <Box component="form" onSubmit={onSubmit}>
-          <TextField
-            required
-            fullWidth
-            label="Username"
-            name="username"
-            id="username"
-            margin="dense"
-            onChange={onUsernameChange}
-          />
-          <TextField
-            required
-            fullWidth
-            label="Password"
-            name="password"
-            id="password"
-            type="password"
-            margin="dense"
-            onChange={onPasswordChange}
-          />
-          <Box display="flex" flexDirection="column" alignItems="center">
-            <Button
-              type="submit"
-              variant="contained"
-              color="secondary"
-              startIcon={<ArrowForwardIosIcon />}
-              size="medium"
-              sx={{ mt: 2 }}
-            >
-              Submit
-            </Button>
+          <Typography color="secondary" variant="h4" fontWeight="bold">Login</Typography>
+          <Box component="form" onSubmit={onSubmit}>
+            <TextField
+              required
+              fullWidth
+              label="Username"
+              name="username"
+              id="username"
+              margin="dense"
+              onChange={onUsernameChange}
+            />
+            {error && (
+              <Typography variant="body2" color="error">
+                {error}
+              </Typography>
+            )}
+            <TextField
+              required
+              fullWidth
+              label="Password"
+              name="password"
+              id="password"
+              type="password"
+              margin="dense"
+              onChange={onPasswordChange}
+            />
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                startIcon={<ArrowForwardIosIcon />}
+                size="medium"
+                sx={{ mt: 2 }}
+              >
+                Submit
+              </Button>
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </Modal>
+      </Modal>
+    </ThemeProvider>
+
   )
 }
 
--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,14 +1,17 @@
-import { Modal, Typography, Box, TextField, Button, IconButton } from "@mui/material"
+import { Modal, Typography, Box, TextField, Button, IconButton, CssBaseline } from "@mui/material"
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import CloseIcon from '@mui/icons-material/Close';
+import { ThemeProvider } from '@mui/material/styles';
 
 import { useState } from "react"
+import axios from "axios";
 
-function Register({ open, onClose }) {
+function Register({ open, onClose, theme }) {
   const [firstName, setFirstName] = useState('')
   const [lastName, setLastName] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
 
   const onFirstNameChange = (e) => {
     setFirstName(e.target.value)
@@ -28,88 +31,107 @@ function Register({ open, onClose }) {
 
   const onSubmit = (e) => {
     e.preventDefault()
-    // to do
-    onClose();
+    const newUser = {
+      firstName,
+      lastName,
+      username,
+      password
+    }
+    axios.post(`http://localhost:4000/user/register`, newUser)
+      .then((res) => { () => onClose() })
+      .catch((err) => {
+        console.log(err)
+        setError(err.response.data.message)
+      });
+
   }
 
   return (
-    <Modal open={open} onClose={onClose}>
-      <Box
-        sx={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-          width: 400,
-          bgcolor: "background.paper",
-          border: "2px solid #000",
-          boxShadow: 24,
-          p: 2,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          gap: "5px"
-        }}
-      >
-        <IconButton color="secondary" onClick={onClose}>
-          <CloseIcon />
-        </IconButton>
+    <ThemeProvider theme={theme}>
+      <Modal open={open} onClose={onClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 2,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "5px"
+          }}
+        >
+          <IconButton color="secondary" onClick={onClose}>
+            <CloseIcon />
+          </IconButton>
 
-        <Typography color="secondary" variant="h4" fontWeight="bold">Register</Typography>
-        <Box component="form" onSubmit={onSubmit}>
-          <TextField
-            required
-            fullWidth
-            label="First Name"
-            name="firstName"
-            id="firstName"
-            margin="dense"
-            onChange={onFirstNameChange}
-          />
-          <TextField
-            required
-            fullWidth
-            label="Last Name"
-            name="lastName"
-            id="lastName"
-            margin="dense"
-            onChange={onLastNameChange}
-          />
-          <TextField
-            required
-            fullWidth
-            label="Username"
-            name="username"
-            id="username"
-            margin="dense"
-            onChange={onUsernameChange}
-          />
-          <TextField
-            required
-            fullWidth
-            label="Password"
-            name="password"
-            id="password"
-            type="password"
-            margin="dense"
-            onChange={onPasswordChange}
-          />
-          <Box display="flex" flexDirection="column" alignItems="center">
-            <Button
-              type="submit"
-              variant="contained"
-              color="secondary"
-              startIcon={<ArrowForwardIosIcon />}
-              size="medium"
-              sx={{ mt: 2 }}
-            >
-              Submit
-            </Button>
+          <Typography color="secondary" variant="h4" fontWeight="bold">Register</Typography>
+          <Box component="form" onSubmit={onSubmit}>
+            <TextField
+              required
+              fullWidth
+              label="First Name"
+              name="firstName"
+              id="firstName"
+              margin="dense"
+              onChange={onFirstNameChange}
+            />
+            <TextField
+              required
+              fullWidth
+              label="Last Name"
+              name="lastName"
+              id="lastName"
+              margin="dense"
+              onChange={onLastNameChange}
+            />
+            <TextField
+              required
+              fullWidth
+              label="Username"
+              name="username"
+              id="username"
+              margin="dense"
+              onChange={onUsernameChange}
+            />
+            {error && (
+              <Typography variant="body2" color="error">
+                {error}
+              </Typography>
+            )}
+            <TextField
+              required
+              fullWidth
+              label="Password"
+              name="password"
+              id="password"
+              type="password"
+              margin="dense"
+              onChange={onPasswordChange}
+            />
+            <Box display="flex" flexDirection="column" alignItems="center">
+              <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                startIcon={<ArrowForwardIosIcon />}
+                size="medium"
+                sx={{ mt: 2 }}
+              >
+                Submit
+              </Button>
+            </Box>
+
           </Box>
-
         </Box>
-      </Box>
-    </Modal>
+      </Modal>
+    </ThemeProvider>
+
   )
 }
 
