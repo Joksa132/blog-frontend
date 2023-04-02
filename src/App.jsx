e9a8b3277633efff8c114267f5587d25d1a1b396
Joksa <nikolajoksimovic419@gmail.com>
2023-04-02 13:04:04 +0200

    added a home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { MantineProvider, Text } from '@mantine/core';
 import './Styles/App.css'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './Pages/Home';
 
 function App() {
 
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Text></Text>
-    </MantineProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
