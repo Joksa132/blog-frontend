b499211e04f4de6a327ee2282dc38862345e6ed3
Joksa <nikolajoksimovic419@gmail.com>
2023-04-03 20:06:45 +0200

    Added delete article function

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -50,6 +50,16 @@ function Home() {
     setShowRegisterModal(false);
   };
 
+  const handleDelete = (id) => {
+    const confirmation = window.confirm("Are you sure you want to delete this post?")
+    confirmation &&
+      axios.delete(`http://localhost:4000/article/delete/${id}`, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
+        .then((res) => {
+          setArticles(articles.filter((article) => article._id !== res.data._id))
+        })
+        .catch((err) => console.log(err))
+  }
+
   return (
     <>
       <Nav handleLoginClick={handleLoginClick} handleRegisterClick={handleRegisterClick} theme={theme} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
@@ -60,7 +70,7 @@ function Home() {
         <Box sx={{ display: "flex", flexDirection: "column", gap: "30px" }}>
           {articles.map((article) => {
             return (
-              <Article key={article._id} article={article} />
+              <Article key={article._id} article={article} handleDelete={() => handleDelete(article._id)} />
             )
           })}
         </Box>
