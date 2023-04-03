96eb3dc3b5b070eb635be3c22f73021a8b6bd486
Joksa <nikolajoksimovic419@gmail.com>
2023-04-03 20:05:03 +0200

    changed some styles and added delete button

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -1,16 +1,28 @@
-import { Typography, Box } from "@mui/material"
-import { Link } from "react-router-dom"
+import { Typography, Box, Button } from "@mui/material"
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
-function Article({ article }) {
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+import { UserContext } from "../../Context/UserContext"
+
+function Article({ article, handleDelete }) {
+  const { user } = useContext(UserContext)
 
   return (
     <Box sx={{ display: "flex", flexDirection: "column" }}>
       <Link to={"/article/" + article._id} style={{ textDecoration: "none" }}>
-        <Typography variant="h3" color="secondary">{article.title}</Typography>
+        <Typography variant="h3" color="#CE93D8">{article.title}</Typography>
       </Link>
-      <Box sx={{ display: "flex", gap: "5px" }}>
-        <Typography variant="body1" color="#CE93D8">{article.createdBy.firstName + " " + article.createdBy.lastName}</Typography>
-        <Typography variant="body1" color="#CE93D8">{new Date(article.createdAt).toLocaleDateString('en-GB')}</Typography>
+      <Box sx={{ display: "flex", gap: "5px", alignItems: "center" }}>
+        <Box sx={{ display: "flex", gap: "5px", alignItems: "center" }}>
+          <Typography variant="body1" color="secondary">{article.createdBy.firstName + " " + article.createdBy.lastName}</Typography>
+          <Typography color="secondary">●</Typography>
+          <Typography variant="body1" color="secondary">{new Date(article.createdAt).toLocaleDateString('en-GB')}</Typography>
+        </Box>
+
+        {user?.userId === article.createdBy._id &&
+          <Button variant="text" color="secondary" size="small" startIcon={<DeleteForeverIcon />} onClick={handleDelete}>Delete</Button>
+        }
       </Box>
     </Box >
   )
