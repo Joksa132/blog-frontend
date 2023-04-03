4f9a07fe644082609b5eb50fab8b12b7fb18f7d3
Joksa <nikolajoksimovic419@gmail.com>
2023-04-03 16:08:29 +0200

    added new article form route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './Styles/App.css'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
+import ArticleForm from './Components/ArticleForm/ArticleForm';
 
 function App() {
 
@@ -9,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-
+        <Route path='/new' element={<ArticleForm />} />
       </Routes>
     </BrowserRouter>
   )
