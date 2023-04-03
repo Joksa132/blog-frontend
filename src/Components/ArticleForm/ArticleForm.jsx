0e34731cc04e40a562e5b1a0a58ef1d8cf2d7dd8
Joksa <nikolajoksimovic419@gmail.com>
2023-04-03 20:05:50 +0200

    added success message and disabled button if logged out

--- src/Components/ArticleForm/ArticleForm.jsx
+++ src/Components/ArticleForm/ArticleForm.jsx
@@ -1,6 +1,7 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Box, Button, CssBaseline, TextField, Typography } from '@mui/material';
+import { Box, Button, CssBaseline, IconButton, TextField, Typography } from '@mui/material';
 import PostAddIcon from '@mui/icons-material/PostAdd';
+import CloseIcon from '@mui/icons-material/Close';
 
 import { useContext, useRef, useState } from "react"
 import { UserContext } from "../../Context/UserContext"
@@ -29,6 +30,7 @@ function ArticleForm() {
   const [value, setValue] = useState('');
   const [title, setTitle] = useState('')
   const editorRef = useRef()
+  const [successMessage, setSuccessMessage] = useState(null)
 
   const onChangeTitle = (e) => {
     setTitle(e.target.value)
@@ -48,8 +50,13 @@ function ArticleForm() {
       .then((res) => {
         setValue('')
         setTitle('')
+        setSuccessMessage("Successfully posted this article!")
       })
       .catch((err) => console.log(err))
+  }
+
+  const handleClose = () => {
+    setSuccessMessage(null)
   }
 
   return (
@@ -90,15 +97,55 @@ function ArticleForm() {
               color="secondary"
             />
             <ReactQuill theme="snow" value={value} onChange={setValue} style={{ width: "100%" }} ref={editorRef} />
-            <Button
-              type='submit'
-              startIcon={<PostAddIcon />}
-              variant='contained'
-              size="large"
-              sx={{ marginTop: "60px" }}
-            >
-              Submit article
-            </Button>
+            {user ?
+              <Button
+                type='submit'
+                startIcon={<PostAddIcon />}
+                variant='contained'
+                size="large"
+                sx={{ marginTop: "60px" }}
+                color="secondary"
+              >
+                Submit article
+              </Button>
+              :
+              <Button
+                type='submit'
+                startIcon={<PostAddIcon />}
+                variant='contained'
+                size="large"
+                sx={{ marginTop: "60px" }}
+                color="secondary"
+                disabled
+              >
+                Submit article
+              </Button>
+            }
+            {successMessage &&
+              <Box sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                bgcolor: "background.paper",
+                border: "2px solid #000",
+                boxShadow: 24,
+                p: 2,
+                width: 300,
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "5px",
+                borderRadius: "15px",
+                marginTop: "20px",
+                flexDirection: "column"
+              }}>
+                <IconButton color="secondary" onClick={handleClose}>
+                  <CloseIcon />
+                </IconButton>
+                <Typography color="secondary" >{successMessage}</Typography>
+              </Box>
+            }
           </Box>
           <Box sx={{ width: "50%" }}>
             <Box display={"flex"} justifyContent={"center"} mb={3}>
