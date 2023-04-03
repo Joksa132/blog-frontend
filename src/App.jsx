ac346804ae73bea0a337a803a52a83d935a25715
Joksa <nikolajoksimovic419@gmail.com>
2023-04-03 20:04:15 +0200

    added specific article page route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './Styles/App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import ArticleForm from './Components/ArticleForm/ArticleForm';
+import ArticlePage from './Pages/ArticlePage/ArticlePage'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<ArticleForm />} />
+        <Route path='/article/:id' element={<ArticlePage />} />
       </Routes>
     </BrowserRouter>
   )
