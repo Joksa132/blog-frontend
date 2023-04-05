9e9f042e4b44e8f7d65d3d20f564f7ee45a1079e
Joksa <nikolajoksimovic419@gmail.com>
2023-04-05 12:46:56 +0200

    removed theme, env variable, added some responsiveness

--- src/Components/ArticleEditForm/ArticleEditForm.jsx
+++ src/Components/ArticleEditForm/ArticleEditForm.jsx
@@ -1,10 +1,8 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Box, Button, CssBaseline, IconButton, TextField, Typography } from '@mui/material';
+import { Box, Button, TextField, Typography } from '@mui/material';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 
 import { useContext, useEffect, useRef, useState } from "react"
 import { UserContext } from "../../Context/UserContext"
-import Nav from "../Nav/Nav"
 import ReactQuill, { Quill } from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import '../../Styles/App.css'
@@ -13,21 +11,7 @@ import { useParams } from 'react-router-dom';
 import Delta from 'quill-delta'
 import { useNavigate } from "react-router-dom";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-});
-
 function ArticleEditForm() {
-  const [isDarkMode, setIsDarkMode] = useState(true)
-  const theme = isDarkMode ? darkTheme : lightTheme
   const { user } = useContext(UserContext)
   const [value, setValue] = useState('');
   const [title, setTitle] = useState('')
@@ -36,7 +20,7 @@ function ArticleEditForm() {
   const { id } = useParams()
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_BackendURL}/article/${id}`)
+    axios.get(`/article/${id}`)
       .then((res) => {
         setTitle(res.data.title)
         const delta = JSON.parse(res.data.content);
@@ -66,7 +50,7 @@ function ArticleEditForm() {
       content: contentJson
     }
 
-    axios.put(`${import.meta.env.VITE_BackendURL}/article/edit/${id}`, editedArticle, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
+    axios.put(`/article/edit/${id}`, editedArticle, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
       .then((res) => {
         navigate("/")
       })
@@ -75,89 +59,85 @@ function ArticleEditForm() {
 
   return (
     <>
-      <Nav theme={theme} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <Box sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginTop: "30px",
+        width: "100%",
+        height: "100%",
+        padding: "20px",
+        gap: "20px"
+      }}>
         <Box sx={{
+          width: "50%",
           display: "flex",
-          justifyContent: "space-between",
-          marginTop: "30px",
-          width: "100%",
-          height: "100%",
-          padding: "20px",
-          gap: "20px"
-        }}>
-          <Box sx={{
-            width: "50%",
-            display: "flex",
-            flexDirection: "column",
-            gap: "10px",
-            alignItems: "center",
-          }}
-            component="form"
-            onSubmit={onSubmit}
-          >
-            <TextField
-              placeholder="Title of the article"
-              required
-              id="title"
-              label="Title"
-              name="title"
-              autoFocus
-              onChange={onChangeTitle}
-              sx={{ width: "400px" }}
-              value={title}
-              fullWidth
+          flexDirection: "column",
+          gap: "10px",
+          alignItems: "center",
+        }}
+          component="form"
+          onSubmit={onSubmit}
+        >
+          <TextField
+            placeholder="Title of the article"
+            required
+            id="title"
+            label="Title"
+            name="title"
+            autoFocus
+            onChange={onChangeTitle}
+            sx={{ width: { md: "400px", sm: "300px" } }}
+            value={title}
+            fullWidth
+            color="secondary"
+          />
+          <ReactQuill theme="snow" value={value} onChange={setValue} style={{ width: "100%" }} ref={editorRef} />
+          {user ?
+            <Button
+              type='submit'
+              startIcon={<PostAddIcon />}
+              variant='contained'
+              size="large"
+              sx={{ marginTop: "60px" }}
               color="secondary"
-            />
-            <ReactQuill theme="snow" value={value} onChange={setValue} style={{ width: "100%" }} ref={editorRef} />
-            {user ?
-              <Button
-                type='submit'
-                startIcon={<PostAddIcon />}
-                variant='contained'
-                size="large"
-                sx={{ marginTop: "60px" }}
-                color="secondary"
-              >
-                Edit article
-              </Button>
-              :
-              <Button
-                type='submit'
-                startIcon={<PostAddIcon />}
-                variant='contained'
-                size="large"
-                sx={{ marginTop: "60px" }}
-                color="secondary"
-                disabled
-              >
-                Submit article
-              </Button>
-            }
-          </Box>
-          <Box sx={{ width: "50%" }}>
-            <Box display={"flex"} justifyContent={"center"} mb={3}>
-              <Typography variant='h4' color="secondary">Preview</Typography>
-            </Box>
-            <Box sx={{
-              border: "2px solid #ce93d8",
-              paddingLeft: "30px",
-              textAlign: "left",
-              overflowWrap: "break-word",
-              minHeight: "130px",
-            }}
             >
-              <Box sx={{ display: "flex", justifyContent: "center", }}>
-                <Typography variant='h3'>{title}</Typography>
-              </Box>
-              <Box
-                dangerouslySetInnerHTML={{ __html: value }}
-              ></Box>
+              Edit article
+            </Button>
+            :
+            <Button
+              type='submit'
+              startIcon={<PostAddIcon />}
+              variant='contained'
+              size="large"
+              sx={{ marginTop: "60px" }}
+              color="secondary"
+              disabled
+            >
+              Submit article
+            </Button>
+          }
+        </Box>
+        <Box sx={{ width: "50%" }}>
+          <Box display={"flex"} justifyContent={"center"} mb={3}>
+            <Typography variant='h4' color="secondary">Preview</Typography>
+          </Box>
+          <Box sx={{
+            border: "2px solid #ce93d8",
+            paddingLeft: "30px",
+            textAlign: "left",
+            overflowWrap: "break-word",
+            minHeight: "130px",
+          }}
+          >
+            <Box sx={{ display: "flex", justifyContent: "center", }}>
+              <Typography variant='h3'>{title}</Typography>
             </Box>
+            <Box
+              dangerouslySetInnerHTML={{ __html: value }}
+            ></Box>
           </Box>
         </Box>
-      </ThemeProvider>
+      </Box>
 
     </>
   )
--- src/Components/ArticleForm/ArticleForm.jsx
+++ src/Components/ArticleForm/ArticleForm.jsx
@@ -1,31 +1,15 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Box, Button, CssBaseline, IconButton, TextField, Typography } from '@mui/material';
+import { Box, Button, IconButton, TextField, Typography } from '@mui/material';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import CloseIcon from '@mui/icons-material/Close';
 
 import { useContext, useRef, useState } from "react"
 import { UserContext } from "../../Context/UserContext"
-import Nav from "../Nav/Nav"
 import ReactQuill, { Quill } from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
 import '../../Styles/App.css'
+import '../../Styles/ArticleForm.css'
 import axios from "axios";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-});
-
 function ArticleForm() {
-  const [isDarkMode, setIsDarkMode] = useState(true)
-  const theme = isDarkMode ? darkTheme : lightTheme
   const { user } = useContext(UserContext)
   const [value, setValue] = useState('');
   const [title, setTitle] = useState('')
@@ -46,7 +30,7 @@ function ArticleForm() {
       content: contentJson
     }
 
-    axios.post(`${import.meta.env.VITE_BackendURL}/article/new`, newArticle, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
+    axios.post(`/article/new`, newArticle, { headers: { Authorization: `Bearer ${localStorage.getItem("token")}` } })
       .then((res) => {
         setValue('')
         setTitle('')
@@ -61,114 +45,111 @@ function ArticleForm() {
 
   return (
     <>
-      <Nav theme={theme} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
+      <Box sx={{
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        justifyContent: "space-between",
+        marginTop: "30px",
+        width: "100%",
+        height: "100%",
+        padding: "20px",
+        gap: "20px"
+      }}>
         <Box sx={{
+          width: "50%",
           display: "flex",
-          justifyContent: "space-between",
-          marginTop: "30px",
-          width: "100%",
-          height: "100%",
-          padding: "20px",
-          gap: "20px"
-        }}>
-          <Box sx={{
-            width: "50%",
-            display: "flex",
-            flexDirection: "column",
-            gap: "10px",
-            alignItems: "center",
-          }}
-            component="form"
-            onSubmit={onSubmit}
-          >
-            <TextField
-              placeholder="Title of the article"
-              required
-              id="title"
-              label="Title"
-              name="title"
-              autoFocus
-              onChange={onChangeTitle}
-              sx={{ width: "400px" }}
-              value={title}
-              fullWidth
+          flexDirection: "column",
+          gap: "10px",
+          alignItems: "center",
+        }}
+          component="form"
+          onSubmit={onSubmit}
+        >
+          <TextField
+            placeholder="Title of the article"
+            required
+            id="title"
+            label="Title"
+            name="title"
+            autoFocus
+            onChange={onChangeTitle}
+            sx={{ width: { md: "400px", sm: "300px" } }}
+            value={title}
+            fullWidth
+            color="secondary"
+          />
+          <ReactQuill theme="snow" value={value} onChange={setValue} style={{ width: "100%" }} ref={editorRef} />
+          {user ?
+            <Button
+              type='submit'
+              startIcon={<PostAddIcon />}
+              variant='contained'
+              size="large"
+              sx={{ marginTop: "60px" }}
               color="secondary"
-            />
-            <ReactQuill theme="snow" value={value} onChange={setValue} style={{ width: "100%" }} ref={editorRef} />
-            {user ?
-              <Button
-                type='submit'
-                startIcon={<PostAddIcon />}
-                variant='contained'
-                size="large"
-                sx={{ marginTop: "60px" }}
-                color="secondary"
-              >
-                Submit article
-              </Button>
-              :
-              <Button
-                type='submit'
-                startIcon={<PostAddIcon />}
-                variant='contained'
-                size="large"
-                sx={{ marginTop: "60px" }}
-                color="secondary"
-                disabled
-              >
-                Submit article
-              </Button>
-            }
-            {successMessage &&
-              <Box sx={{
-                position: "absolute",
-                top: "50%",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                bgcolor: "background.paper",
-                border: "2px solid #000",
-                boxShadow: 24,
-                p: 2,
-                width: 300,
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "5px",
-                borderRadius: "15px",
-                marginTop: "20px",
-                flexDirection: "column"
-              }}>
-                <IconButton color="secondary" onClick={handleClose}>
-                  <CloseIcon />
-                </IconButton>
-                <Typography color="secondary" >{successMessage}</Typography>
-              </Box>
-            }
-          </Box>
-          <Box sx={{ width: "50%" }}>
-            <Box display={"flex"} justifyContent={"center"} mb={3}>
-              <Typography variant='h4' color="secondary">Preview</Typography>
-            </Box>
-            <Box sx={{
-              border: "2px solid #ce93d8",
-              paddingLeft: "30px",
-              textAlign: "left",
-              overflowWrap: "break-word",
-              minHeight: "130px",
-            }}
             >
-              <Box sx={{ display: "flex", justifyContent: "center", }}>
-                <Typography variant='h3'>{title}</Typography>
-              </Box>
-              <Box
-                dangerouslySetInnerHTML={{ __html: value }}
-              ></Box>
+              Submit article
+            </Button>
+            :
+            <Button
+              type='submit'
+              startIcon={<PostAddIcon />}
+              variant='contained'
+              size="large"
+              sx={{ marginTop: "60px" }}
+              color="secondary"
+              disabled
+            >
+              Submit article
+            </Button>
+          }
+          {successMessage &&
+            <Box sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              bgcolor: "background.paper",
+              border: "2px solid #000",
+              boxShadow: 24,
+              p: 2,
+              width: 300,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              padding: "5px",
+              borderRadius: "15px",
+              marginTop: "20px",
+              flexDirection: "column"
+            }}>
+              <IconButton color="secondary" onClick={handleClose}>
+                <CloseIcon />
+              </IconButton>
+              <Typography color="secondary" >{successMessage}</Typography>
             </Box>
+          }
+        </Box>
+        <Box sx={{ width: "50%" }}>
+          <Box display={"flex"} justifyContent={"center"} mb={3}>
+            <Typography variant='h4' color="secondary">Preview</Typography>
+          </Box>
+          <Box sx={{
+            border: "2px solid #ce93d8",
+            paddingLeft: "30px",
+            textAlign: "left",
+            overflowWrap: "break-word",
+            minHeight: "130px",
+          }}
+          >
+            <Box sx={{ display: "flex", justifyContent: "center", }}>
+              <Typography variant='h3'>{title}</Typography>
+            </Box>
+            <Box
+              dangerouslySetInnerHTML={{ __html: value }}
+            ></Box>
           </Box>
         </Box>
-      </ThemeProvider>
+      </Box>
 
     </>
   )
