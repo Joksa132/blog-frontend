25e98af12c90c238dc6da0198c4dc25e5dffac6e
Joksa <nikolajoksimovic419@gmail.com>
2023-04-05 12:26:44 +0200

    moved modal functions and theme provider to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,70 @@
 import './Styles/App.css'
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './Pages/Home/Home';
 import ArticleForm from './Components/ArticleForm/ArticleForm';
 import ArticlePage from './Pages/ArticlePage/ArticlePage'
 import ArticleEditForm from './Components/ArticleEditForm/ArticleEditForm';
+import Nav from './Components/Nav/Nav';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { useState } from 'react';
+
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+const lightTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(true)
+  const theme = isDarkMode ? darkTheme : lightTheme
+  const [showLoginModal, setShowLoginModal] = useState(false)
+  const [showRegisterModal, setShowRegisterModal] = useState(false)
+
+  const handleLoginModalClose = () => {
+    setShowLoginModal(false);
+  };
+
+  const handleRegisterModalClose = () => {
+    setShowRegisterModal(false);
+  };
+
+  const handleRegisterClick = () => {
+    setShowRegisterModal(true);
+  };
+
+  const handleLoginClick = () => {
+    setShowLoginModal(true);
+  };
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<ArticleForm />} />
-        <Route path='/article/:id' element={<ArticlePage />} />
-        <Route path='/edit/:id' element={<ArticleEditForm />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Nav isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} handleLoginClick={handleLoginClick} handleRegisterClick={handleRegisterClick} />
+
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Login open={showLoginModal} onClose={handleLoginModalClose} />
+          <Register open={showRegisterModal} onClose={handleRegisterModalClose} />
+
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/new' element={<ArticleForm />} />
+            <Route path='/article/:id' element={<ArticlePage />} />
+            <Route path='/edit/:id' element={<ArticleEditForm />} />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </>
+
   )
 }
 
