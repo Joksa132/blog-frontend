1b0e1dd4edf385d8cf5602b6c79e633d2d9411e2
Joksa <nikolajoksimovic419@gmail.com>
2023-04-05 12:26:09 +0200

    added baseurl for axios

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { UserProvider } from './Context/UserContext';
+import axios from "axios";
+
+axios.defaults.baseURL = import.meta.env.VITE_BackendURL
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
