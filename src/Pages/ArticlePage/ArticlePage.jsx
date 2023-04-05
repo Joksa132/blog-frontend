19bd111628db986b39d0692af9c6af038581279a
Joksa <nikolajoksimovic419@gmail.com>
2023-04-05 12:45:46 +0200

    removed theme, modals, env variable

--- src/Pages/ArticlePage/ArticlePage.jsx
+++ src/Pages/ArticlePage/ArticlePage.jsx
@@ -1,39 +1,18 @@
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { Box, Button, Container, CssBaseline, TextField, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 
 import axios from "axios";
-import { useContext, useEffect, useState } from "react"
-import { UserContext } from "../../Context/UserContext"
-import Nav from "../../Components/Nav/Nav"
+import { useEffect, useState } from "react"
 import { useParams } from 'react-router-dom';
 import { QuillDeltaToHtmlConverter } from 'quill-delta-to-html';
-import Login from '../../Components/Login/Login';
-import Register from '../../Components/Register/Register';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
-
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-});
 
 function ArticlePage() {
-  const [isDarkMode, setIsDarkMode] = useState(true)
-  const theme = isDarkMode ? darkTheme : lightTheme
   const [article, setArticle] = useState({})
   const { id } = useParams()
   const [contentHtml, setContentHtml] = useState('')
-  const [showLoginModal, setShowLoginModal] = useState(false)
-  const [showRegisterModal, setShowRegisterModal] = useState(false)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    axios.get(`${import.meta.env.VITE_BackendURL}/article/${id}`)
+    axios.get(`/article/${id}`)
       .then((res) => {
         setArticle(res.data)
         console.log(JSON.parse(res.data.content))
@@ -45,53 +24,31 @@ function ArticlePage() {
       .catch((err) => console.log(err))
   }, [])
 
-  const handleLoginClick = () => {
-    setShowLoginModal(true);
-  };
-
-  const handleLoginModalClose = () => {
-    setShowLoginModal(false);
-  };
-
-  const handleRegisterClick = () => {
-    setShowRegisterModal(true);
-  };
-
-  const handleRegisterModalClose = () => {
-    setShowRegisterModal(false);
-  };
-
   return (
     <>
-      <Nav handleLoginClick={handleLoginClick} handleRegisterClick={handleRegisterClick} theme={theme} isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-      <Login open={showLoginModal} onClose={handleLoginModalClose} theme={theme} />
-      <Register open={showRegisterModal} onClose={handleRegisterModalClose} theme={theme} />
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {!loading ?
-          <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "30px" }}>
-            <Box sx={{ display: "flex", justifyContent: "center", marginTop: "40px" }}>
-              <Typography variant='h2' color="secondary">{article.title}</Typography>
-            </Box>
-            <Box sx={{
-              marginTop: "20px", border: "2px solid #ce93d8", padding: "30px", textAlign: "left", overflowWrap: "break-word",
-            }} >
-              <Box sx={{ display: "flex", gap: "5px", marginBottom: "10px" }}>
-                <Typography color={"#CE93D8"}>{article?.createdBy?.firstName + " " + article?.createdBy?.lastName}</Typography>
-                <Typography color="#CE93D8">{" ● "}</Typography>
-                <Typography color={"#CE93D8"}>{new Date(article.createdAt).toLocaleDateString('en-GB')}</Typography>
-              </Box>
-              <Box dangerouslySetInnerHTML={{ __html: contentHtml }}>
-
-              </Box>
-            </Box>
-
-          </Container>
-          : <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "50px" }}>
-            <Typography variant='h4' color="secondary">LOADING</Typography>
+      {!loading ?
+        <Container sx={{ display: "flex", flexDirection: "column", alignItems: "center", marginBottom: "30px" }}>
+          <Box sx={{ display: "flex", justifyContent: "center", marginTop: "40px" }}>
+            <Typography variant='h2' color="secondary">{article.title}</Typography>
           </Box>
-        }
-      </ThemeProvider >
+          <Box sx={{
+            marginTop: "20px", border: "2px solid #ce93d8", padding: "30px", textAlign: "left", overflowWrap: "break-word",
+          }} >
+            <Box sx={{ display: "flex", gap: "5px", marginBottom: "10px" }}>
+              <Typography color={"#CE93D8"}>{article?.createdBy?.firstName + " " + article?.createdBy?.lastName}</Typography>
+              <Typography color="#CE93D8">{" ● "}</Typography>
+              <Typography color={"#CE93D8"}>{new Date(article.createdAt).toLocaleDateString('en-GB')}</Typography>
+            </Box>
+            <Box dangerouslySetInnerHTML={{ __html: contentHtml }}>
+
+            </Box>
+          </Box>
+
+        </Container>
+        : <Box sx={{ display: "flex", justifyContent: "center", alignItems: "center", marginTop: "50px" }}>
+          <Typography variant='h4' color="secondary">LOADING</Typography>
+        </Box>
+      }
     </>
   )
 }
