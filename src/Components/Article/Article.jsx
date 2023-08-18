ffb18953339fd0f0e9a2f7746cf715585128109d
Joksa132 <nikolajoksimovic419@gmail.com>
2023-08-18 15:57:23 +0200

    change article styles

--- src/Components/Article/Article.jsx
+++ src/Components/Article/Article.jsx
@@ -9,25 +9,66 @@ import { UserContext } from "../../Context/UserContext"
 function Article({ article, handleDelete }) {
   const { user } = useContext(UserContext)
 
+  function formatDate(dateString) {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Date(dateString).toLocaleDateString('en-GB', options);
+  }
+
   return (
-    <Box sx={{ display: "flex", flexDirection: "column" }}>
+    <Box sx={{ display: "flex", flexDirection: "column", gap: "7px" }}>
       <Link to={"/article/" + article._id} style={{ textDecoration: "none" }}>
-        <Typography variant="h3" color="#b90ac2">{article.title}</Typography>
+        <Typography
+          variant="h2"
+          sx={{ fontSize: "28px", fontWeight: "900" }}
+          color='primary'
+        >
+          {article.title}
+        </Typography>
       </Link>
-      <Box sx={{ display: "flex", gap: "5px", alignItems: "center" }}>
-        <Box sx={{ display: "flex", gap: "5px", alignItems: "center" }}>
-          <Typography variant="body1" color="secondary">{article.createdBy.firstName + " " + article.createdBy.lastName}</Typography>
-          <Typography color="secondary">●</Typography>
-          <Typography variant="body1" color="secondary">{new Date(article.createdAt).toLocaleDateString('en-GB')}</Typography>
+      <Box sx={{ display: "flex", gap: "20px", alignItems: "center", }}>
+        <Box sx={{ display: "flex", gap: "10px", alignItems: "center" }}>
+          <Typography
+            color="secondary"
+            sx={{ fontSize: "14px", fontFamily: "Merriweather, sans-serif" }}
+          >
+            {article.createdBy.firstName + " " + article.createdBy.lastName}
+          </Typography>
+          <Typography
+            color="secondary"
+            fontSize="small"
+          >
+            ●
+          </Typography>
+          <Typography
+            color="secondary"
+            sx={{ fontSize: "14px", fontFamily: "Merriweather, sans-serif" }}
+          >
+            {formatDate(article.createdAt)}
+          </Typography>
         </Box>
 
         {user?.userId === article.createdBy._id &&
-          <>
-            <Button variant="text" color="secondary" size="small" startIcon={<DeleteForeverIcon />} onClick={handleDelete}>Delete</Button>
+          <Box sx={{ display: "flex", gap: "5px", alignItems: "center" }}>
+            <Button
+              color="secondary"
+              size="small"
+              sx={{ padding: 0, fontFamily: "Merriweather, sans-serif", textTransform: "none", fontSize: "13px" }}
+              startIcon={<DeleteForeverIcon />}
+              onClick={handleDelete}
+            >
+              Delete
+            </Button>
             <Link to={"/edit/" + article._id} style={{ textDecoration: "none" }}>
-              <Button variant="text" color="secondary" size="small" startIcon={<EditIcon />}>Edit</Button>
+              <Button
+                color="secondary"
+                size="small"
+                sx={{ padding: 0, fontFamily: "Merriweather, sans-serif", textTransform: "none", fontSize: "13px" }}
+                startIcon={<EditIcon />}
+              >
+                Edit
+              </Button>
             </Link>
-          </>
+          </Box>
         }
       </Box>
     </Box >
